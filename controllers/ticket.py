a1b4cfec107ed25de70b8c36990ffd2d477c1f1a
nxlouie <nxlouie@umich.edu>
2016-10-08 07:55:43 -0400

    add new ticket done

--- controllers/ticket.py
+++ controllers/ticket.py
@@ -9,5 +9,6 @@ def ticket_route():
     return render_template("ticket.html")
   if request.method=="POST":
     cur = db.cursor()
-    cur.execute('INSERT INTO Ticket VALUES ()')
+    print request.form.get('last')
+    cur.execute('INSERT INTO Ticket VALUES (NULL,%s,%s,%s,%s,%s,%s,NULL)',(request.form.get('first'),request.form.get('last'),request.form.get('phone'),request.form.get('email'),request.form.get('major'),request.form.get('grad')))
     return redirect(request.url)
