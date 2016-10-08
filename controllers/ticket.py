eea971974c93a889f029151b58aef96dd3f67451
nxlouie <nxlouie@umich.edu>
2016-10-08 06:12:45 -0400

    gonna NAP brb

--- controllers/ticket.py
+++ controllers/ticket.py
@@ -3,6 +3,11 @@ from extensions import db
 
 ticket = Blueprint('ticket', __name__, template_folder='templates')
 
-@ticket.route('/ticket')
+@ticket.route('/ticket', methods=['GET','POST'])
 def ticket_route():
-	return render_template("ticket.html")
+  if request.method=="GET":
+    return render_template("ticket.html")
+  if request.method=="POST":
+    cur = db.cursor()
+    cur.execute('INSERT INTO Ticket VALUES ()')
+    return redirect(request.url)
