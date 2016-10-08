a3cbac052b04919a4fc44dedcc1c1af463370896
nxlouie <nxlouie@umich.edu>
2016-10-08 05:32:49 -0400

    rename route

--- controllers/ticket.py
+++ controllers/ticket.py
@@ -4,5 +4,5 @@ from extensions import db
 ticket = Blueprint('ticket', __name__, template_folder='templates')
 
 @ticket.route('/ticket')
-def ticket_create_route():
+def ticket_route():
 	return render_template("ticket.html")
