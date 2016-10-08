0e64b65194f9417d1474bfe4c31593ff180e072f
nxlouie <nxlouie@umich.edu>
2016-10-08 09:54:41 -0400

    can now add and remove from queue

--- controllers/ticket.py
+++ controllers/ticket.py
@@ -10,5 +10,5 @@ def ticket_route():
   if request.method=="POST":
     cur = db.cursor()
     print request.form.get('last')
-    cur.execute('INSERT INTO Ticket VALUES (NULL,%s,%s,%s,%s,%s,%s,NULL,NULL)',(request.form.get('first'),request.form.get('last'),request.form.get('phone'),request.form.get('email'),request.form.get('major'),request.form.get('grad')))
+    cur.execute('INSERT INTO Ticket VALUES (NULL,%s,%s,%s,%s,%s,%s,NULL,NULL,0)',(request.form.get('first'),request.form.get('last'),request.form.get('phone'),request.form.get('email'),request.form.get('major'),request.form.get('grad')))
     return redirect(request.url)
